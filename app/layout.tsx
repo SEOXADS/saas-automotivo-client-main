21a4c8cb5ef15fa069f58450ece80023b6e20d9d
Gabriel Kemmer <gabrielkemmer@k9agency.digital>
2026-01-26 19:13:00 -0300

    Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,8 @@ export default function RootLayout({
         {/* End Google Tag Manager */}
 
         {/* Favicons */}
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
         <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
@@ -42,7 +44,6 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
         <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
         {/* End Favicons */}
 
